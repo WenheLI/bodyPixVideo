57108aae04d74026ea1f82ecca5e4d714d9b0bf6
WenheLI <wl1508@nyu.edu>
2021-02-28 18:12:33 +0800

    add

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
+import '@tensorflow/tfjs-backend-webgl';
+import * as tf from '@tensorflow/tfjs-core';
 import * as bodyPix from '@tensorflow-models/body-pix'; 
 
 const CAMERA = {
     audio: false,
     video: {
-        height: {ideal: 315},
-        width: {ideal: 560},
+        height: {ideal: 200},
+        width: {ideal: 200},
         facingMode: 'user'
     }
 }
@@ -20,8 +22,6 @@ const main = async () => {
         return;
     }
 
-    const mediaStream = await navigator.mediaDevices.getUserMedia(CAMERA);
-
     const net = await bodyPix.load({
         architecture: 'MobileNetV1',
         outputStride: 16,
@@ -29,7 +29,63 @@ const main = async () => {
         quantBytes: 2
     });
 
+    const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+    const video: HTMLVideoElement = document.getElementById('video') as HTMLVideoElement;
 
+    document.getElementById('pannel').addEventListener('click', (e) => {
+        // @ts-ignore
+        switch(e.target.id) {
+            case 'top-left':
+                canvas.style.left = '8';
+                canvas.style.top = '0';
+                break;
+            case 'top-right':
+                canvas.style.left = '368';
+                canvas.style.top = '0';
+                break;
+            case 'bottom-left':
+                canvas.style.left = '8';
+                canvas.style.top = '115';
+                break;
+            case 'bottom-right':
+                canvas.style.left = '368';
+                canvas.style.top = '115';
+                break;
+            default:
+                break;
+        }
+    })
+
+    const mediaStream = await navigator.mediaDevices.getUserMedia(CAMERA);
+
+    video.srcObject = mediaStream;
+
+    const offCanvas: HTMLCanvasElement = document.getElementById('side-canvas') as HTMLCanvasElement;
+    
+    const offCtx = offCanvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
+    const draw = async () => {
+        offCtx.drawImage(video, 0, 0);
+        const res = await net.segmentPerson(offCanvas);
+        document.getElementById('i').style.display = 'none';
+        tf.tidy(() => {
+            const maskTensor = tf.tensor3d(res.data, [200, 200, 1]);
+            const imageTensor = tf.browser.fromPixels(offCanvas);
+            const t1 = tf.mul(imageTensor, maskTensor) as tf.Tensor3D;
+            const t2 = tf.concat([t1, tf.mul(maskTensor, 255)], 2);
+            t2.data().then((rawData) => {
+                const rawImageData = new ImageData(new Uint8ClampedArray(rawData), 200, 200);
+                ctx.putImageData(rawImageData, 0, 0);
+                ctx.scale(-1, 1);
+                ctx.translate(-canvas.width, 0);
+            });
+        })
+
+        requestAnimationFrame(draw);
+    }
+    setTimeout(() => {
+        requestAnimationFrame(draw);
+    });
 }
 
 main();
